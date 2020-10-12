daa89051b6ff2751c04369f3bf23ec6b2a0a7a03
Eric Ribeiro <eric.ribeiro@live.com>
2020-10-11 21:43:00 -0500

    Added fade out

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class ContextualMessageController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float fadeMessageDuration = 1;
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
     void Start()
@@ -20,12 +21,22 @@ public class ContextualMessageController : MonoBehaviour
     private IEnumerator ShowMessage(string message, float duration) {
         canvasGroup.alpha = 1;
         messageText.text = message;
+        
         yield return new WaitForSeconds(duration);
+        float fadeElapsedTime = 0;
+        float fadeStartTime = Time.time; 
+        while (fadeElapsedTime < fadeMessageDuration)
+        {
+            fadeElapsedTime = Time.time - fadeStartTime;
+            canvasGroup.alpha = 1 - fadeElapsedTime / fadeMessageDuration;
+            yield return null;
+        }
         canvasGroup.alpha = 0;
     }
    
     private void OnContextualMessageTriggered(string message, float messageDuration)
     {
+        StopAllCoroutines();
         StartCoroutine(ShowMessage(message, messageDuration));
 
     }
